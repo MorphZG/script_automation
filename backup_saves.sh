ff29218c1884acbc1e56e24c33191c6982582fe0
MorphZG <zoran1607@gmail.com>
2024-03-07 00:24:31 +0100

    minor text formating

--- backup_saves.sh
+++ backup_saves.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 # Steam games on Linux Ubuntu
-# Backup save files
-# Baldur's Gate and Pillars of Eternity, 
+# -- Backup save files:
+# Baldur's Gate,
+# Pillars of Eternity, 
+# PillarsOfEternity II
 
 tar -cvf save_games.tar "/home/zoran/.local/share/Baldur's Gate - Enhanced Edition" "/home/zoran/.local/share/PillarsOfEternity" "/home/zoran/.local/share/PillarsOfEternityII"
