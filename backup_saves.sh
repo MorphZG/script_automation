c36af366ca25088448f66794d205a6fb618c9bd8
MorphZG <zoran1607@gmail.com>
2024-05-14 21:03:50 +0200

    add path to HOI4 save directory

--- backup_saves.sh
+++ backup_saves.sh
@@ -5,5 +5,6 @@
 # Baldur's Gate,
 # Pillars of Eternity, 
 # PillarsOfEternity II
+# Hearts of Iron IV
 
-tar -cvf save_games.tar "/home/zoran/.local/share/Baldur's Gate - Enhanced Edition" "/home/zoran/.local/share/PillarsOfEternity" "/home/zoran/.local/share/PillarsOfEternityII"
+tar -cvf save_games.tar "/home/zoran/.local/share/Paradox Interactive/Hearts of Iron IV/save games" "/home/zoran/.local/share/Baldur's Gate - Enhanced Edition" "/home/zoran/.local/share/PillarsOfEternity" "/home/zoran/.local/share/PillarsOfEternityII"
